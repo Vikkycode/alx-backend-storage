87f58f92d5ab38ddf79572a415845c5da85f45d1
Victor <vicky.robilson5528@gmail.com>
2024-10-18 05:10:17 +0100

    changed fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- Lists the number of fans per country origin of a band in descending orders --
+-- Lists the number of fans per country origin of a band in descending order --
 SELECT origin, COUNT(fan_id) AS nb_fans
 FROM metal_bands
 GROUP BY origin
