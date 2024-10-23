555fb0e7004828b644a0aebc04919c072317b59f
Victor <vicky.robilson5528@gmail.com>
2024-10-23 23:41:09 +0100

    update exercise file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-""" Cache class """
+"""
+Cache class to interact with Redis
+"""
 import redis
 import uuid
-
+from typing import Union
 
 class Cache:
     """
-    Cache class using Redis
+    Cache class to interact with Redis
     """
     def __init__(self):
-        """
-        Initialize Redis client and flush the instance
-        """
+        """Initialize the Redis client and flush the database"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
-        """
-       Store data in Redis using a random key
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store the input data in Redis using a random key and return the key"""
+        key = str(uuid.uuid4())  # Generate a random key using uuid4
+        self._redis.set(key, data)  # Store data in Redis
+        return key  # Return the random key
+
