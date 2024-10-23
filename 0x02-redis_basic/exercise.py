6d3ac2ab67632213c84e6f3695203906593594c7
Victor <vicky.robilson5528@gmail.com>
2024-10-24 00:02:49 +0100

    4 exercise file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Cache class to interact with Redis
+Cache class with count_calls decorator
+to count method calls
 """
 import redis
 import uuid
@@ -8,87 +9,35 @@ from typing import Union, Callable
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count how many times a
+    method is called."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function that increments
+        the call count in Redis."""
+
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
 class Cache:
-    """
-    Cache class to interact with Redis
-    """
     def __init__(self):
-        """Initialize the Redis client and flush the database"""
+        """Initialize the Redis client and
+        flush the database."""
         self._redis = redis.Redis()
         self._redis.flushdb()
-
-    def count_calls(self, method: Callable) -> Callable:
-        """
-        Decorator to count the number of times a method is called.
-
-        Args:
-            method (Callable): The method to be decorated.
-
-        Returns:
-            Callable: The wrapped method with call counting functionality.
-        """
-        key = method.__qualname__
-
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            """
-            Wrapper function to increment the
-            call count and call the original method.
-            """
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-        return wrapper
 
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis using a random
-        key and return the key
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        key and return the key."""
+        key = str(uuid.uuid4())  # Generate a random key
+        self._redis.set(key, data)  # Store data in Redis
+        return key  # Return the generated key
 
-    def get(self, key: str, fn: Callable = None) -> (
-        Union[str, bytes, int, float, None]
-    ):
-        """
-        Retrieve data from Redis based on the given key.
-
-        Args:
-            key (str): The key to retrieve the data.
-            fn (Callable, optional): A callable function to apply
-            to the retrieved data. Defaults to None.
-
-        Returns:
-            Union[str, bytes, int, float, None]: The retrieved data
-            after applying the callable function, or None if the key
-            doesn't exist.
-        """
-        value = self._redis.get(key)
-        if fn:
-            return fn(value)
-        return value
-
-    def get_str(self, key: str) -> str:
-        """
-        Retrieve data from Redis as a string.
-
-        Args:
-            key (str): The key to retrieve the data.
-
-        Returns:
-            str: The retrieved data as a string.
-        """
-        return self.get(key, lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str) -> int:
-        """
-        Retrieve data from Redis as an integer.
-
-        Args:
-            key (str): The key to retrieve the data.
-
-        Returns:
-            int: The retrieved data as an integer.
-        """
-        return self.get(key, int)
+    def get(self, key: str) -> Union[str, None]:
+        """Retrieve data from Redis by key."""
+        return self._redis.get(key)
