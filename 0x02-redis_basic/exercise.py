c6e1339b8d60cf82acbf0e86dda4b7b9313ac54d
Victor <vicky.robilson5528@gmail.com>
2024-10-23 23:44:38 +0100

    2 exercise file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Cache class to interact with Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     """
@@ -21,3 +21,42 @@ class Cache:
         self._redis.set(key, data)  # Store data in Redis
         return key  # Return the random key
 
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis based on the given key.
+
+        Args:
+            key (str): The key to retrieve the data.
+            fn (Callable, optional): A callable function to apply to the retrieved data. Defaults to None.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data after applying the callable function, or None if the key doesn't exist.
+        """
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieve data from Redis as a string.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            str: The retrieved data as a string.
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve data from Redis as an integer.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            int: The retrieved data as an integer.
+        """
+        return self.get(key, int)
